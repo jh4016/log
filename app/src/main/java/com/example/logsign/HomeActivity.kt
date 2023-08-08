385cba78b2410b1f0612627f365af4fead0ad39a
jh4016 <wlsgur_e@naver.com>
2023-08-08 14:45:22 +0900

    디자인 추가

--- app/src/main/java/com/example/logsign/HomeActivity.kt
+++ app/src/main/java/com/example/logsign/HomeActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 
 class HomeActivity : AppCompatActivity() {
     @SuppressLint("SetTextI18n")
--- app/src/main/java/com/example/logsign/SignInActivity.kt
+++ app/src/main/java/com/example/logsign/SignInActivity.kt
@@ -20,6 +20,8 @@ class SignInActivity : AppCompatActivity() {
         val password = findViewById<EditText>(R.id.pas_1)
         val get_id=intent.getStringExtra("ID")
         val get_name=intent.getStringExtra("name")
+
+
         btn2.setOnClickListener {//회원가입
             val intent = Intent(this, Sign_Up_Activity::class.java)
             startActivity(intent)
@@ -27,7 +29,9 @@ class SignInActivity : AppCompatActivity() {
         btn1.setOnClickListener {//로그인
             if (id.text.toString().isNotEmpty() && password.text.toString().isNotEmpty()) {
                 val intent = Intent(this, HomeActivity::class.java)
-
+                intent.putExtra("ID", get_id.toString()) // Extra로 아이디 전달
+                intent.putExtra("name", get_name.toString()) // Extra로 Name 전달
+                startActivity(intent)
                 Toast.makeText(this, "로그인 되었습니다", Toast.LENGTH_SHORT).show()
             } else {
                 Toast.makeText(this, "아이디 비밀번호를 확인해주세요", Toast.LENGTH_SHORT).show()
